5fb8441ae2cc7ba4db348a0fd2f71abab3f7f920
Gilmore Davidson <gilmoreorless@gmail.com>
2016-02-09 19:00:12 +1100

    Switched test runner to mocha
    This is what the plugin boilerplate previously used and is a hell of a lot faster than compiling ES6 that's only in test files.
    
    Test timings using ava (+ Babel included): 125 seconds first run, 15 seconds subsequent runs.
    Test timings using mocha: 0.4 seconds.

--- test.js
+++ test.js
@@ -1,16 +1,60 @@
-import postcss from 'postcss';
-import test    from 'ava';
+var postcss = require('postcss');
+var expect  = require('chai').expect;
 
-import plugin from './';
+var plugin = require('./');
 
-function run(t, input, output, opts = { }) {
-    return postcss([ plugin(opts) ]).process(input)
-        .then( result => {
-            t.same(result.css, output);
-            t.same(result.warnings().length, 0);
-        });
-}
+var test = function (input, output, opts, done) {
+    if (typeof opts === 'function' && done === undefined) {
+        done = opts;
+        opts = {};
+    }
+    postcss([ plugin(opts) ]).process(input).then(function (result) {
+        expect(result.css).to.eql(output);
+        expect(result.warnings()).to.be.empty;
+        done();
+    }).catch(function (error) {
+        done(error);
+    });
+};
 
-test('does something', t => {
-    return run(t, 'a{ }', 'a{ }', { });
+var testGradient = function (input, output, opts, done) {
+    var prefix = '.test{ background-image:linear-gradient( ';
+    var suffix = ' ); }';
+    input = prefix + input + suffix;
+    output = prefix + output + suffix;
+    return test(input, output, opts, done);
+};
+
+/* global describe, it */
+describe('postcss-gradient-transparency-fix', function () {
+
+    it('ignores non-gradient transparent values', function (done) {
+        return test('a{ background:transparent }', 'a{ background:transparent }', done);
+    });
+
+    it('corrects single rgb() value', function (done) {
+        return testGradient('rgb(255,0,0), transparent', 'rgb(255,0,0), rgba(255,0,0,0)', done);
+    });
+
 });
+
+
+/*
+
+TO TEST:
+
+- rgb()
+- rgba()
+- hsl()
+- hsla()
+- hex (3)
+- hex (6)
+- hex (8)
+- named colours
+
+- one value (colour, trans)
+- one value (trans, colour)
+- two values
+- different gradient types
+- different properties (wherever image is supported)
+*/
